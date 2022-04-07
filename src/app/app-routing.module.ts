86aabbf583ad522c0be121797e529fd2cb08fa69
Mateusz Dragan <33156@student.pwsztar.edu.pl>
2022-04-07 16:07:24 +0200

    lab_12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {BlogComponent} from "./components/blog/blog.component";
 import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
+import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: BlogComponent
-  },
+    component: BlogHomeComponent},
   {
     path: 'blog/detail/:id',
     component: BlogItemDetailsComponent
@@ -19,4 +19,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
